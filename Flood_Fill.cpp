7f0f498f42cb28aa28544c39e4d255e4a8531d19
GoYabai <leduchuyqazzaq@gmail.com>
2026-04-16 00:25:11 +0700

    Day 24

--- Flood_Fill.cpp
+++ Flood_Fill.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 using namespace std;
 
+//DFS
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
@@ -33,5 +34,43 @@ public:
         dfs(image, sr, sc - 1, color, org);
         dfs(image, sr, sc + 1, color, org);
         
+    }
+};
+
+//BFS
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int org = image[sr][sc];
+        if (org == color) {
+            return image;
+        }
+        int m = image.size();
+        int n = image[0].size();
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        image[sr][sc] = color;
+        
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+        
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i]; 
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && image[nr][nc] == org) {
+                    image[nr][nc] = color;
+                    q.push({nr, nc});
+                }
+            }
+        }
+        return image;
     }
 };
